6c3b95253bd5e129203528b3b2a973d823e9b277
dave tush <mustaphadavid98@gmail.com>
2024-01-02 21:50:36 +0100

    main page

--- lib/main.dart
+++ lib/main.dart
@@ -1,16 +1,31 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:new_easylector_app/Pages/General_page.dart';
+import 'package:new_easylector_app/providers.dart';
+import 'package:new_easylector_app/video_player.dart';
 import 'package:provider/provider.dart';
+import 'Pages/nav bar pages/Home_page.dart';
+import 'Pages/test_pages.dart';
 import 'Sign up and login pages/Sign_up_page.dart';
+import 'Sign up and login pages/login_page.dart';
+import 'chat.dart';
+import 'chatmessage.dart';
+import 'controller/message_controller.dart';
 import 'foundation/theme_house/theme.dart';
 
 Future<void> main() async {
-  // WidgetsFlutterBinding.ensureInitialized();
   // await SystemChrome.setPreferredOrientations([
   //   DeviceOrientation.portraitUp,
   //   DeviceOrientation.portraitDown,
   // ]);
-  runApp(MyApp());
+  runApp(MultiProvider(
+    providers: [
+      ChangeNotifierProvider<MessageController>(
+        create: (_) => MessageController(),
+      ),
+    ],
+    child: const MyApp(),
+  ));
 }
 
 class MyApp extends StatefulWidget {
@@ -22,18 +37,32 @@ class MyApp extends StatefulWidget {
 
 class _MyAppState extends State<MyApp> {
   @override
-  Widget build(BuildContext context) => ChangeNotifierProvider(
-        create: (context) => ThemeProvider(),
-        builder: (context, _) {
-          final themeProvider = Provider.of<ThemeProvider>(context);
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      // themeMode: themeProvider.themeMode,
+      theme: MyThemes.lightTheme,
+      darkTheme: MyThemes.darkTheme,
+      home: const MainPage(),
+    );
+  }
+}
 
-          return MaterialApp(
-            debugShowCheckedModeBanner: false,
-            themeMode: themeProvider.themeMode,
-            theme: MyThemes.lightTheme,
-            darkTheme: MyThemes.darkTheme,
-            home: const signup_page(),
-          );
-        },
-      );
+class MainPage extends StatelessWidget {
+  const MainPage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Scaffold(
+        body:
+            // chatting()
+            // signup_page()
+            // LoginPage()
+            // providers()
+            // );
+            // TestPage(subject: 'mth',)
+        // VideoPlayers(),
+            general_page()
+    );
+  }
 }
